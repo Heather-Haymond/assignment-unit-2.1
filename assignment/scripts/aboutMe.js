11791155a4cd92bb6b92be55b3a3e013f50849d9
Heather Haymond <hheatherjean11@gmail.com>
2024-03-03 15:42:45 -0600

    first commit. commit terminal practice. commmit more often

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,46 +1,63 @@
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name
-let firstName = '';
+let firstName = 'Heather';
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
-
+let lastName = 'Haymond';
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 //     (remember, you already have variables for this, can you use those?)
 //     Console log the value of `fullName`
-
+let fullName = 'Heather Haymond';
+console.log('fullName')
 // 4 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
 //     Console log the value of `luckyNumber`
-
+let luckyNumber = 11;
 // 5 - Create a variable `introduction` and using the variables from above,
 //     give it the value of:
 //     'My name is (full name), and I think (lucky number) is a winner!'.
 //     Refer back to the videos if you need help with this one.
 //     Console log the value of `introduction`
-
+let introduction = "My name is " + fullName + ", and I think " + luckyNumber + " is a winner!";
+console.log('introduction')
 // 6 - Create a variable named `adventurous` and set it to a boolean value (true or false)
 //     Console log the value of `adventurous`
-
+let adventurous = true
+console.log('adventerous')
 // 7 - Create a variable named `food`, and set its value to a string of your favorite food
 //     Console log the value of `food`
-
+let food = 'fruit'
+console.log('food')
 // 8 - Create a variable called `pets` and set it to the value of the number of pets you have
 //     Console log the value of `pets`
-
+let pets = 1
+console.log(pets)
 // 9 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
-
+let friendsPets = 0
 // 10 - Add two pets to your `pets` variable
-
+friendsPets += 2;
 // 11 - Create a constant variable called `allowedPets` and set it to a number value of your choice
-
+let allowedPets = 4
 // 12 - Create a variable called `result`. Create a conditional:
 //      if adventurous is true, set `result` to be "Adventures are great!", 
 //      if it's not true,  set `result` to be "How about we stay home?"
 //      Console log the value of `result`
-
+let result;  // set variable without assigned value
+if (adventurous) {  // conditional if else statement sets value of variable
+  result = "Adventures are great!";
+} else {
+  result = "How about we stay home?";
+}
+console.log(result); //check expected result
 // 13 - Create a variable called `diceRoll` and set it to the value of "Try again later.".
 //      Create a compound conditional: 
 //      if luckyNumber is 2 and adventurous is true,
 //      set `diceRoll` to be "Roll the dice!"
 //      Console log the value of `diceRoll`
+
+let diceRoll = "Try again later." //default value set to variable 
+if (luckyNumber === 2 && adventurous) {
+  diceRoll = "Roll the dice!"; //updates conditional
+}
+console.log(diceRoll); //checks for expected result
 
 // 14 - Create a variable called `petStatus`. 
 //      Write a conditional that covers the following:
@@ -51,14 +68,23 @@ let firstName = '';
 //      if the value of `pets` is greater than the value of `allowedPets`.
 //      set `petStatus` to the value of "Oh no, I have too many pets!"
 
-
-// STRETCH GOALS:
+let petStatus; // set variable without assigned value
+if (pets < allowedPets){ //statement assigns value
+  petStatus = "I can have more pets";
+} else if (pets === allowedPets){
+  petStatus = "I have enough pets";
+} else if (pets < allowedPets){ //else statements do not take conditions *reminder*
+  petStatus = "Oh no, I have too many pets!";
+}
+console.log(petStatus); //expected result check
+ // STRETCH GOALS:
 
 // 15 - Make a variable called `mostPets` and a conditional that
 //      correctly checks the `pets` and `friendsPets` variables, and
 //      assigns the highest value to `mostPets`. There's several possibilities --
 //      be sure to think through all the scenarios. 
 //      console.log `mostPets` after the conditional has run.
+let mostPets; 
 
 // 16 - Make a variable called `luckyResult`
 //      Write a *switch* statement that sets `luckyResult` to:
